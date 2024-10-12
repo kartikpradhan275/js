863cf6b67b8bb2a7b650e07c224b6248d9c401e2
kartikpradhan275 <154908651+kartikpradhan275@users.noreply.github.com>
2024-10-12 09:48:49 +0000

    note added for var

--- .devcontainer/01_basic/variables.js
+++ .devcontainer/01_basic/variables.js
@@ -2,6 +2,9 @@ const accountId =144553
 let accountEmail= "kartik@gmail.com"
 var accountPassword = "Jharkhand123"
 accountCity ="jharkhand";
+/*
+please don't use var
+*/
 
 accountEmail="kp@g.com";
 accountPassword ="212";
