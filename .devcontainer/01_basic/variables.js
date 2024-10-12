8f9620f288a8075d1e7a27060ec168cbb5e2a3a3
kartikpradhan275 <154908651+kartikpradhan275@users.noreply.github.com>
2024-10-12 09:45:37 +0000

    variables code

---
+++ .devcontainer/01_basic/variables.js
@@ -0,0 +1,10 @@
+const accountId =144553
+let accountEmail= "kartik@gmail.com"
+var accountPassword = "Jharkhand123"
+accountCity ="jharkhand";
+
+accountEmail="kp@g.com";
+accountPassword ="212";
+accountCity ="sonua"
+console.table([accountId,accountEmail,accountPassword]);
+
